1bd2bd66b1e31a62ede8e77104c9e748f58695ca
sunshineplan <sunshineplan@users.noreply.github.com>
2021-12-13 16:46:21 +0800

    Setup dependabot

--- go.mod
+++ go.mod
@@ -1,5 +1,10 @@
 module useragent
 
-go 1.16
+go 1.17
 
 require github.com/anaskhan96/soup v1.2.4
+
+require (
+	golang.org/x/net v0.0.0-20211209124913-491a49abca63 // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
--- useragent.go
+++ useragent.go
@@ -10,37 +10,40 @@ import (
 	"github.com/anaskhan96/soup"
 )
 
-var api, filename, agent string
+var (
+	api      = flag.String("url", "", "URL")
+	filename = flag.String("filename", "user-agent", "filename")
+)
 
-func getAgent() error {
-	soup.Header("User-Agent", "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)")
-	body, err := soup.Get(api)
+func main() {
+	flag.Parse()
+
+	agent, err := getAgent()
 	if err != nil {
-		return err
+		log.Fatal(err)
+	}
+
+	if err := os.WriteFile(*filename, []byte(agent), 0666); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func getAgent() (string, error) {
+	soup.Header("User-Agent", "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)")
+	body, err := soup.Get(*api)
+	if err != nil {
+		return "", err
 	}
 
 	ua := soup.HTMLParse(body).Find("span", "class", "code")
 	if ua.Error != nil {
-		return ua.Error
+		return "", ua.Error
 	}
 
-	if agent = ua.Text(); !strings.Contains(agent, "Chrome") {
-		return fmt.Errorf("bad result: %s", agent)
+	agent := ua.Text()
+	if !strings.Contains(agent, "Chrome") {
+		return "", fmt.Errorf("bad result: %s", agent)
 	}
 
-	return nil
-}
-
-func main() {
-	flag.StringVar(&api, "url", "", "URL")
-	flag.StringVar(&filename, "filename", "user-agent", "filename")
-	flag.Parse()
-
-	if err := getAgent(); err != nil {
-		log.Fatal(err)
-	}
-
-	if err := os.WriteFile(filename, []byte(agent), 0666); err != nil {
-		log.Fatal(err)
-	}
+	return agent, nil
 }
