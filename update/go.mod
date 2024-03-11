0c87000a4c05a7935a91e165c80ebc7a45afa77f
sunshineplan <sunshineplan@users.noreply.github.com>
2024-03-11 10:07:19 +0800

    Update

--- update/go.mod
+++ update/go.mod
@@ -1,19 +1,19 @@
 module main
 
-go 1.21
+go 1.22
+
+require github.com/sunshineplan/chrome v1.1.10
 
 require (
-	github.com/sunshineplan/node v1.0.4
-	github.com/sunshineplan/useragent v0.0.0-00010101000000-000000000000
-)
-
-require (
-	github.com/antchfx/htmlquery v1.3.0 // indirect
-	github.com/antchfx/xpath v1.2.3 // indirect
-	github.com/ericchiang/css v1.3.0 // indirect
-	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	golang.org/x/net v0.18.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	github.com/chromedp/cdproto v0.0.0-20240202021202-6d0b6a386732 // indirect
+	github.com/chromedp/chromedp v0.9.5 // indirect
+	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.3.2 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	golang.org/x/sys v0.16.0 // indirect
 )
 
 replace github.com/sunshineplan/useragent => ../
--- update/main.go
+++ update/main.go
@@ -1,85 +1,18 @@
 package main
 
 import (
-	"errors"
-	"flag"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
-	"regexp"
+	"runtime"
 
-	"github.com/sunshineplan/node"
-	"github.com/sunshineplan/useragent"
+	"github.com/sunshineplan/chrome"
 )
 
-var list = map[string]string{
-	"windows": "on Windows 10",
-	"darwin":  "on macOS",
-	"linux":   "on Linux",
-	"ios":     "on iOS",
-	"android": "on Android",
-}
-
-var api = flag.String("url", "", "URL")
-
 func main() {
-	flag.Parse()
-
-	html, err := fetch()
-	if err != nil {
+	ua := chrome.UserAgent()
+	fmt.Print(ua)
+	if err := os.WriteFile(runtime.GOOS, []byte(ua), 0644); err != nil {
 		log.Fatal(err)
 	}
-	if err := parse(html); err != nil {
-		log.Fatal(err)
-	}
-	f, err := os.Create("README.md")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-	for _, i := range useragent.SupportedOS() {
-		fmt.Fprintf(f, "## %s\n", i)
-		fmt.Fprintln(f, "```")
-		fmt.Fprintln(f, list[i])
-		fmt.Fprintln(f, "```")
-	}
-}
-
-func fetch() (html node.Node, err error) {
-	req, err := http.NewRequest("GET", *api, nil)
-	if err != nil {
-		return
-	}
-	req.Header.Set("User-Agent", "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)")
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return
-	}
-	defer resp.Body.Close()
-
-	html, err = node.Parse(resp.Body)
-	return
-}
-
-func parse(html node.Node) error {
-	for k, v := range list {
-		if h2 := html.Find(0, node.H2, node.String(regexp.MustCompile(v))); h2 != nil {
-			if span := h2.Find(node.Next, node.Span, node.Class("code")); span != nil {
-				ua := span.GetText()
-				if err := os.WriteFile(k, []byte(ua), 0644); err != nil {
-					return err
-				}
-				list[k] = ua
-			} else {
-				return errors.New("no found")
-			}
-		} else {
-			if h1 := html.Find(0, node.H1); h1 != nil {
-				return errors.New(h1.GetText())
-			}
-			return errors.New("blocked")
-		}
-	}
-	return nil
 }
